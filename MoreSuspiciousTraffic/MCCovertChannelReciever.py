a92f9aa37d2df4fcd3678ca2c90b53ce3259fc21
cadetfitz <cadetfitz@users.noreply.github.com>
2016-11-11 21:46:31 -0600

    Update MCCovertChannelReciever.py
    Commented Code

--- MoreSuspiciousTraffic/MCCovertChannelReciever.py
+++ MoreSuspiciousTraffic/MCCovertChannelReciever.py
@@ -1,10 +1,11 @@
+__author__ = 'LtDan'
 import random
 from socket import *
 import time
 import base64
 
-
-
+# Morse Code mapping 
+# Could not match flag{} format as { and } could not be found, used () instead
 CODE = {'A': '.-',     'B': '-...',   'C': '-.-.',
         'D': '-..',    'E': '.',      'F': '..-.',
         'G': '--.',    'H': '....',   'I': '..',
@@ -19,12 +20,13 @@ CODE = {'A': '.-',     'B': '-...',   'C': '-.-.',
 		'4': '....-',  '5': '.....',  '6': '-....',
 		'7': '--...',  '8': '---..',  '9': '----.'
         }
-
+# Decode message
 inverseCODE = dict((v,k) for (k,v) in CODE.items())
-
+# Open socket
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', 23284))
-start = time.time()
+serverSocket.bind(('', 23284)) # Wikipedia says port 23284 is registered to TimeTracker. Funny easter egg telling you to look at the time.
+start = time.time() # Record start time
+# Lame variables
 tp = 0
 x = 1
 y = 1
@@ -39,37 +41,37 @@ while True:
     #message = message.upper()
     #print message
     #print t
-    
+	
+    # 
     if (t-tp)<0.1:
 	x += 1
 	tp = t
     elif (t - tp)<0.6:
-	if x == 6:
+	if x == 6: # Long/Dash
 	    char += '-'
 	    #print "long"
-	elif x == 3:
+	elif x == 3: # Short/Dot
 	    char += '.'
 	    #print "short"
 	x = 1
 	tp = t
-	#print "\n"
+	
     elif (t-tp)<3.5:
 	print "Recieving Message..."
-	if x == 6:
+	if x == 6: # Long/Dash
 	    char += '-'
             #print "long"
-        elif x == 3:
+        elif x == 3: # Short/Dot
 	    char += '.'
             #print "short"
         x = 1
         tp = t
-	#print char
+	
 	if char != '':
-	    #print inverseCODE[char]
+	    # Added decoded character to decoded message
 	    decode = decode + inverseCODE[char]
-	    #print "Decoded message: " + decode + '\n'
 	char = ''
-	#print "BREAK\n"
+	
     else:
 	print "Decoded message: " + decode + '\n'
 	x = 1
@@ -78,9 +80,9 @@ while True:
 	char = ''
 	decode = ""
 	
+    #  Recieve message (Message doesn't matter, just when it was recieved)
     message, address = serverSocket.recvfrom(1024)
-    
-
+    # Send ACK
     ack = base64.b64encode(bytes("ACK"))
     serverSocket.sendto(ack, address)
 
