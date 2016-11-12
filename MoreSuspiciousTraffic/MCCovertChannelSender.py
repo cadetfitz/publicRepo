28fab9d4704269b38a1ef7d13eff6e15efa50e29
cadetfitz <cadetfitz@users.noreply.github.com>
2016-11-11 21:46:20 -0600

    Update MCCovertChannelSender.py
    Commented code

--- MoreSuspiciousTraffic/MCCovertChannelSender.py
+++ MoreSuspiciousTraffic/MCCovertChannelSender.py
@@ -1,4 +1,4 @@
-__author__ = 'fitzgd2'
+__author__ = 'LtDan'
 
 import time
 from socket import *
@@ -6,13 +6,16 @@ import sys
 import base64
 import random
 
-
+# Enter IP Address and Port as arguments
 ipAddress = str(sys.argv[1])
 port = int(str(sys.argv[2]))
+
 print "IP Address: %s"%(ipAddress)
 print "Port: %d" %(port)
 print " "
 
+# Morse Code mapping 
+# Could not match flag{} format as { and } could not be found, used () instead
 CODE = {'A': '.-',     'B': '-...',   'C': '-.-.', 
         'D': '-..',    'E': '.',      'F': '..-.',
         'G': '--.',    'H': '....',   'I': '..',
@@ -28,7 +31,7 @@ CODE = {'A': '.-',     'B': '-...',   'C': '-.-.',
 		'7': '--...',  '8': '---..',  '9': '----.'
         }
 
-
+# Function to encode message in Morse
 def morseEncoder(msg):
 	result = []
 	i = 0
@@ -36,38 +39,39 @@ def morseEncoder(msg):
 		result.append(CODE[char.upper()])
 		i += 1
 	return result
-
+# Function to send encoded message
 def encodeSend(msg, clientSocket, addr):
 	for char in msg:
 		for i in char:
-			if i == '.':
+			if i == '.': # Short/Dot
 				sendShort(clientSocket, addr)
 				print "short"
-			elif i == '-':
+			elif i == '-': # Long/Dash
 				sendLong(clientSocket, addr)
 				print "long"
 			else:
 				print "error"
-			time.sleep(0.5)
-		time.sleep(1)
+			time.sleep(0.5) # Wait 0.5 seconds between dot/dash
+		time.sleep(1) # Wait 1 second between characters
 		print "\n"
-
+# Function to send short/dot
 def sendShort(clientSocket, addr):
-	message = base64.b64encode(bytes(sendMe()))
+	message = base64.b64encode(bytes(sendMe())) # B64 encode message (because why not?)
 	x = 0
-	while x < 3:
+	while x < 3: # Send 3 times
 		clientSocket.sendto(message, addr)
 		x += 1
-
+# Function to send long/dash
 def sendLong(clientSocket, addr):
-	message = base64.b64encode(bytes(sendMe()))
+	message = base64.b64encode(bytes(sendMe())) # B64 encode message
 	x = 0
-	while x < 6:
+	while x < 6: # Send 6 times
 		clientSocket.sendto(message, addr)
 		x += 1
 
 def sendMe():
-	rand = random.randint(0,3)
+	# Previous version sent one of 4 different messages to increase difficulty
+	rand = random.randint(0,3) 
 	if rand == 0:
 		return "nottheflag"
 	elif rand == 1:
@@ -82,33 +86,27 @@ def main():  # Main
 	clientSocket = socket(AF_INET, SOCK_DGRAM)
 	clientSocket.settimeout(1) #wait up to one second to receive reply
 	message = 'FLAG(H4CK7H3PL4N37)'    #message sent
-	eof = 'EOF'
-	addr = (ipAddress, port) #loopback address on port 12000
+	eof = 'EOF' # End
+	addr = (ipAddress, port) 
 
-	encoded = morseEncoder(message)
+	encoded = morseEncoder(message) # Encode message
 	print encoded
 	
 	start = time.clock() # get start time, use time.clock for precision
-	encodeSend(encoded, clientSocket, addr)
+	encodeSend(encoded, clientSocket, addr) # Send encoded message
 
-	#print 'Packet number: %d' % (pings)
-	
-	#print "Start time: %f" % (start)
-	clientSocket.sendto(eof, addr)  #send ping
+	# Debug stuff
+	clientSocket.sendto(eof, addr)  #send End char
 	time.sleep(4)
-	clientSocket.sendto(eof, addr)  #send ping
+	clientSocket.sendto(eof, addr)  #send End char
 	try:
 		data, server = clientSocket.recvfrom(1024)  #get reply
 		end = time.clock()   #get end time
-		#print "End time: %f" % (end)
 		elapsed = (end - start) *1000    #get time elapsed in milliseconds
-		#print 'Data sent: %s' % (data)
-		#print 'Packet number: %d' % (pings)
-		#print 'Time elapsed: %f' % (elapsed) # as float for precision
-		print 'PING to <%s> : <%f>' % (ipAddress, elapsed) #Prints Ping <ipAddress> <sequence number> <elapsed time>
+		print 'Message to <%s> : Time elapsed = <%f>' % (ipAddress, elapsed) 
 	except timeout: #get timeout and print
-		print 'PING to <%s> LOST' % (ipAddress)
-	#time.sleep(1) #sleep for one second, waits one second in between pings
+		print 'Message to <%s> LOST' % (ipAddress)
+	
 
 	sys.exit()
 
